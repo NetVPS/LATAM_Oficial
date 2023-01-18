0a159a66a812ed54b4bce5330609a3c8a33cfcec
Kalix1 <31332664+NetVPS@users.noreply.github.com>
2023-01-18 16:31:30 -0600

    18-01-23

--- Fixs Remotos/HELP.sh
+++ Fixs Remotos/HELP.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Soporte Remoto (HELP)
 echo "" >/dev/null 2>&1
-wget https://www.dropbox.com/s/v791c3oxiqi53hu/SOPORTE.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
+wget https://raw.githubusercontent.com/NetVPS/LATAM_Oficial/main/Fixs%20Remotos/SOPORTE.sh -O /usr/bin/SOPORTE > /dev/null 2>&1
 chmod +x /usr/bin/SOPORTE > /dev/null 2>&1
