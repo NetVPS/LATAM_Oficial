b282a58912813df5407c04f0470e3ffbe97141d1
Kalix1 <31332664+NetVPS@users.noreply.github.com>
2023-01-26 22:49:41 -0600

    Update menu.sh

--- Codigo-Base/menu.sh
+++ Codigo-Base/menu.sh
@@ -700,7 +700,6 @@ ajuste_in() {
     read x
     [[ $x = @(n|N) ]] && msg -bar && return
     echo -e ""
-    fun_bar "service ssh restart"
     wget -O /etc/pam.d/common-password https://raw.githubusercontent.com/NetVPS/LATAM_Oficial/main/Ejecutables/common-password
     chmod +rwx /etc/pam.d/common-password
     fun_bar "service ssh restart"
